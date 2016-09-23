a10fd90d33cd2e99f526e34f4e10f323f29b00a9
Hasnain Baxamoosa <hbaxamoosa@Hasnains-MacBook-Pro.local>
2016-09-23 09:41:16 -0500

    Initial commit

---
+++ app/src/main/java/com/alienvault/threatscanner/ThreatScanner.java
@@ -0,0 +1,39 @@
+package com.alienvault.threatscanner;
+
+import android.app.Application;
+import android.content.Context;
+import com.facebook.stetho.Stetho;
+import com.firebase.client.Firebase;
+import timber.log.Timber;
+
+/**
+ * Created by hbaxamoosa on 9/23/16.
+ */
+
+public class ThreatScanner extends Application {
+
+    private static Context context;
+
+    public static Context getAppContext() {
+        return ThreatScanner.context;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ThreatScanner.context = getApplicationContext();
+        Firebase.setAndroidContext(this);
+
+        //Including Jake Wharton's Timber logging library
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+            Timber.v("Timber.plant(new Timber.DebugTree());");
+        } else {
+            // Timber.plant(new CrashReportingTree());
+        }
+
+        // Facebook Stetho
+        Stetho.initializeWithDefaults(this);
+    }
+}
